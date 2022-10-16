64c30baff8eb7a1581a4b0178f42c374bae050cd
Sabrina Yose Amelia <sabrina.amelia@student.matanauniversity.ac.id>
2022-10-16 23:11:54 +0700

    Jadwal Header

--- page/prodi/jadwal_hdr/index.php
+++ page/prodi/jadwal_hdr/index.php
@@ -1,1 +1,51 @@
-<h1>Buat Grid Jadwal_Hdr Disini (Berdasarkan no_prodi)</h1>
+<?php 
+    $result = $mysqli->query("SELECT * FROM jadwal_hdr h JOIN prodi p ON h.no_prodi=p.no_prodi");
+?>
+<div class="card">
+    <div class="card-header row justify-content-between">
+        <h5 class="col">Daftar Jadwal Header</h5>
+        <div class="col-lg-2 col-md-3 col-sm-4 row justify-content-end">
+            <a href="/<?= $folder ?>/prodi/jadwal_hdr/new">
+                <button type="button" class="btn btn-primary">
+                    <span class="tf-icons bx bx-plus"></span>&nbsp; Tambah
+                </button>
+            </a>
+        </div>
+    </div>
+    <div class="table-responsive text-nowrap">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>No Jadwal Header</th>
+                    <th>Nama Prodi</th>
+                    <th>Semester</th>
+                    <th>Tahun Akademik</th>
+                    <th>Tahun Kurikulum</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody class="table-border-bottom-0">
+                <?php 
+                    while ($row = $result->fetch_assoc()) {
+                ?>
+                <tr>
+                    <td><?= $row['no_jadwal_hdr'] ?></td>
+                    <td><?= $row['nama_prodi'] ?></td>
+                    <td><?= $row['semester'] ?></td>
+                    <td><?= $row['tahun_akademik'] ?></td>
+                    <td><?= $row['tahun_kurikulum'] ?></td>
+                    <td>
+                        <a href="/<?= $folder ?>/prodi/jadwal_hdr/edit?no_jadwal_hdr=<?= $row['no_jadwal_hdr'] ?>">
+                            <button type="button" class="btn btn-icon btn-outline-primary">
+                                <span class="tf-icons bx bx-edit-alt"></span>
+                            </button>
+                        </a>
+                    </td>
+                </tr>
+                <?php
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
+</div>
